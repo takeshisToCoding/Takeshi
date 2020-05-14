f7e713dd17566dad04c88f80d1f14c9e23788873
edgarVazquez43 <edgarvazquez1403@gmail.com>
2020-05-14 16:05:09 -0500

    Delete m2r dependency.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 import sys
 import os
 import sphinx_rtd_theme
-import m2r
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
